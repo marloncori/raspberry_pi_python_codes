b056311bd735e66d747802147ad80f65e917143e
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-02-10 16:27:55 +0100

    Create simple_server.py

---
+++ simple_server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "\n\033[1;35m======================\033[0m\n \033[1;34mHello, Flask!\n This is a simple server.\n\tEnjoy it :D\n\033[0m"
+
+if __name__ == '__main__':
+  try:
+    app.run(debug=True, host='0.0.0.0')
+    
+  except (KeyboardInterrupt, SystemExit):
+    print(" [Server has been shut down.]")
+    sys.exit(1)
