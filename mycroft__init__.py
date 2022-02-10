1e72680e23b0aa0587c4c969091441fca167f547
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-02-10 19:30:03 +0100

    Create mycroft__init__.py

---
+++ mycroft__init__.py
@@ -0,0 +1,25 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+
+import requests
+
+class MyRobot(MycroftSkill):
+  def __init__(self):
+    super().__init__()
+    self.base_url = self.settings.get("base_url")
+   
+  @intent_handler(IntentBuilder("")
+                 .require("robot")
+                 .require("test"))
+  def handle_test(self, msg):
+    try:
+      requests.post(self.base_url + "/run/test")
+      self.speak_dialog('robot')
+      self.speak_dialog('test')
+    except:
+      self.speak_dialog("UnableToReach")
+      LOG.exception("Unable to reach the robot")
+ 
+def create_skill():
+  return MyRobot()
